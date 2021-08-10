15f6e1f1971b9c1b0dc97a134cd9a7062af6d624
Md Javedul Ferdous <mdjavedulferdous@gmail.com>
2021-08-09 23:59:29 -0400

    Aug 9

--- Extension/New/content.js
+++ Extension/New/content.js
@@ -1,32 +1,50 @@
-var x, i, fList = [], childList = [], temp = [],sortList = [],sentSortList=[], pageList=[], sentPageList = [];
+var fList = [],
+    childList = [],
+    temp = [],
+    sortList = [],
+    sentSortList = [],
+    pageList = [],
+    sentPageList = [];
 
 //Filter
-x = document.querySelectorAll('[data-attribute="filter"]');
-for (i = 0; i < x.length; i++) {
-    fList.push({id:x[i].childNodes[1].textContent.trim()})
+let x = document.querySelectorAll('[data-attribute="filter"]');
+for (let i = 0; i < x.length; i++) {
+    fList.push({
+        id: x[i].childNodes[1].textContent.trim()
+    })
 }
+for (let i = 0; i < 6; i+=1) {
+    if ((x[i].childNodes[3]).textContent != [] ) {
+        temp.push({name:x[i].childNodes[1].textContent.trim(),id: (x[i].childNodes[3]).textContent.replace(/(.*?\s.*?\s)/g, '$1' + '\n').split('\n\n').filter(word => word.trim().length > 0)}
+        );
+        }
+}
+console.log(temp);
 
 //Sort
 sortList = document.querySelectorAll('[data-attribute="sort"]');
-sentSortList.push({id:(sortList[0].textContent.replace(/(.*?\s.*?\s)/g, '$1'+'\n')).split('\n\n').filter(word => word.trim().length > 0)});
+sentSortList.push({
+    id: (sortList[0].textContent.replace(/(.*?\s.*?\s)/g, '$1' + '\n')).split('\n\n').filter(word => word.trim().length > 0)
+});
 
 //Page
 pageList = document.querySelectorAll('[data-attribute="page"]');
-sentPageList.push({id:(pageList[0].textContent.replace(/(.*?\s.*?\s)/g, '$1'+'\n')).split('\n\n').filter(word => word.trim().length > 0)});
-
-//Search
-/*searchList = document.querySelectorAll('[data-attribute="search"]');
-console.log(searchList[0]);
-console.log(searchList[0].action);
-console.log(searchList[0].method);
-let temprary = searchList[0].children;
-console.log(temprary.querySelector('value'))
-*/
-chrome.runtime.onConnect.addListener((port) => {           
-        port.postMessage({
-            filterList: fList, 
-            sentSortListAttribute: sentSortList, 
-            sentPageListAttribute: sentPageList
-        });
+sentPageList.push({
+    id: (pageList[0].textContent.replace(/(.*?\s.*?\s)/g, '$1' + '\n')).split('\n\n').filter(word => word.trim().length > 0)
 });
 
+//Search
+searchList = document.querySelectorAll('[data-attribute="search"]');
+/*console.log(searchList[0]);
+console.log(searchList[0].getAttribute('action'));
+console.log(searchList[0].getAttribute('method'));*/
+
+
+chrome.runtime.onConnect.addListener((port) => {
+    port.postMessage({
+        filterList: fList,
+        sentSortListAttribute: sentSortList,
+        sentPageListAttribute: sentPageList,
+        dropDownFilter:temp
+    });
+});
--- Extension/New/popup.js
+++ Extension/New/popup.js
@@ -1,30 +1,43 @@
 //var response = ["Amazon Prime", "Department", "Avg. Customer Review", "Brand", "Price", "Office Products Material", "Subscribe & Save", "From Our Brands", "Packaging Option", "Certification", "Amazon Global Store"];
  //var response = [{id:"Amazon Prime"}, {id:"Department"}, {id:"Avg. Customer Review"}, {id:"Brand"}];
  var emptyList = [], emptySortList = [], emptyPageList=[]; 
+ let domList = document.getElementById("displayList");
 //document.addEventListener('DOMContentLoaded', function() {
 registerEvent();
+
 function registerEvent(){
     // Clicking the Filter Button
     var checkfilterButton= document.getElementById('filterButton');
     checkfilterButton.addEventListener('click', function() {
-        filterList = getthelist();
-        //console.log(filterList);
+    
+    filterList = getthelist();
     chrome.tabs.getSelected(null, function(tab) {
         for (i = 0; i < filterList.length; i++) 
-        {   var hLink = document.createElement("a");
-            hLink.text = filterList[i].id;
-            hLink.target = '_blank';
-            hLink.href = 'https://www.amazon.com/';
-            var node = document.createElement("LI");
-            node.appendChild(hLink);
-            document.getElementById("displayList").appendChild(node); ///append Item
-            //var node = document.createElement("LI");  // Create a <li> node
-            //var textnode = document.createTextNode(filterList[i].id); // Create a text node
-            //node.appendChild(textnode); 
-            //document.getElementById("displayList").appendChild(node); ///append Item
-            console.log([i+1],"Filter");
-            console.log(window.location);
+        {   var cbutton = document.createElement("input");
+            cbutton.setAttribute('type', 'submit');
+            cbutton.setAttribute('value', filterList[i].name);
+            cbutton.setAttribute("class", "collapsible");
+            cbutton.setAttribute("id", "collapsible");
+            cbutton.setAttribute("data-toggle", "collapse");
+            cbutton.setAttribute("data-target", "#demo");
+         
+            let node = document.createElement("Li");
+            let para = document.createElement("li");
+            for(let j=0; j<filterList.length; j++)
+            {
+                para.text=filterList[j].id;
+            }
+            
+            cbutton.appendChild(para);
+            node.appendChild(cbutton);
+            domList.appendChild(node);
         }	
+        var coll = document.getElementsByClassName("collapsible");
+        for (let i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function() {
+                console.log(filterList[i].id);
+            })
+        }
       }
       
       );
@@ -41,7 +54,7 @@ function registerEvent(){
             hLink.href = 'https://www.amazon.com/';
             var node = document.createElement("LI");
             node.appendChild(hLink);
-            document.getElementById("displayList").appendChild(node); ///append Item
+            domList.appendChild(node); ///append Item
             //var node = document.createElement("LI");  // Create a <li> node
             //var textnode = document.createTextNode(filterList[i].id); // Create a text node
             //node.appendChild(textnode); 
@@ -78,22 +91,23 @@ function registerEvent(){
     //Clicking the Search Button
     var checkSearchButton= document.getElementById('searchButton');
     checkSearchButton.addEventListener('click', function() {
-        /*var form = document.createElement('form');
+        var form = document.createElement('form');
         form.setAttribute('action', 'https://www.amazon.com/s/ref=nb_sb_noss_2');
-        form.setAttribute('method', 'post');
+        form.setAttribute('method', 'GET');
+        form.setAttribute('role', 'search');
 
         var text_field = document.createElement('input');
         text_field.setAttribute('type', 'text');
-        text_field.setAttribute('value', 'enter something here...');
+        text_field.setAttribute('value', '');
 
         var button = document.createElement('input');
         button.setAttribute('type', 'submit');
-        button.setAttribute('value', 'Go!');
+        button.setAttribute('value', 'Search');
 
         form.appendChild(text_field);
         form.appendChild(button);
 
-        document.body.appendChild(form);*/
+        document.body.appendChild(form);
     }, false);
 
 }
@@ -108,12 +122,12 @@ function getthelist(){
 
             port.onMessage.addListener((response) => {
                 //console.log(response.filterList);
-                for (i = 0; i < response.filterList.length; i++) {
-                    emptyList.push(response.filterList[i]);
+                for (i = 0; i < response.dropDownFilter.length; i++) {
+                    emptyList.push(response.dropDownFilter[i]);
                 }
                 //emptyList.push(response.filterList);
                 //console.log(emptyList);
-                console.log("From Popup script",response.filterList);
+                console.log("From Popup script",response.dropDownFilter);
             });        
         });
     return emptyList;
@@ -130,7 +144,7 @@ function getthesortlist(){
                 emptySortList.push(response.sentSortListAttribute[0].id[i]);
             }
             console.log("From Popup script",emptySortList);
-        });        
+        });      
     });
 return emptySortList;
 }
@@ -151,7 +165,18 @@ function getthepagelist(){
     });
 return emptyPageList;
 }
+function testingFunction()
+{
+    for (let i = 0; i < filterList.length; i++) 
+        {   let aLink = document.createElement("a");
+            aLink.text = filterList[i].id;
 
+            var node = document.createElement("LI");
+            node.appendChild(aLink);
+            console.log(filterList[i].id); 
+            return node;          
+        }	
+}
 getthesortlist();
 getthelist();
 getthepagelist();
