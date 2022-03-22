b561a0a0de99dd117c3a44f4b50cc1fbbefd8635
Md Javedul Ferdous <mdjavedulferdous@gmail.com>
2022-03-21 23:39:50 -0400

    extension

--- Extension/background.js
+++
@@ -1,7 +0,0 @@
-console.log("Background script running!");
-console.log(document.location);
-chrome.browserAction.onClicked.addListener(establishPort);
-function establishPort(tab) {
-    let port = chrome.tabs.connect(tab.id, {name: "establish_connection"});
-    port.postMessage({action: "openModal"});
-}
