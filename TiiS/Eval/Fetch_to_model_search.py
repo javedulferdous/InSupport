e9382d7dc00fb83e2756d506459d4ffb5f5ed527
Md Javedul Ferdous <mdjavedulferdous@gmail.com>
2022-07-08 16:19:15 -0400

    Update Fetch_to_model_search.py

--- TiiS/Eval/Fetch_to_model_search.py
+++ TiiS/Eval/Fetch_to_model_search.py
@@ -20,17 +20,26 @@ def evaluation(Search_X_test,searchname,target_names,search_URL_name):
             print("==========================")
     df = pd.DataFrame(y_score_1) 
     return (df)
-Search_searchpath = '/Users/mdjavedulferdous/Desktop/TiiS/Code/result/search/model_csv/AmericanEagle.html.csv'
-Search_csv_df = pd.read_csv(Search_searchpath)
-search_URL_name = Search_csv_df[["URL name"]]
-Search_X_test = Search_csv_df[["search_innertext", "search_attribute", "Number_of_search_word","search_button_attribute_value","is_button"]]
-searchname = "/Users/mdjavedulferdous/Desktop/TiiS/Code/search_model.sav"
-target_names = ['Non-Search', 'Search']
-search_URL_name = (search_URL_name["URL name"][0].split('/')[-1]).split('.')[0]
-nameCSV = search_URL_name+'.csv'
+def search_extract_model(_name_):
+    Search_searchpath = '/Users/mdjavedulferdous/Desktop/TiiS/Code/result/search/model_csv/AmericanEagle.html.csv'
+    Search_csv_df = pd.read_csv(Search_searchpath)
+    search_URL_name = Search_csv_df[["URL name"]]
+    Search_X_test = Search_csv_df[["search_innertext", "search_attribute", "Number_of_search_word","search_button_attribute_value","is_button"]]
+    searchname = "/Users/mdjavedulferdous/Desktop/TiiS/Code/search_model.sav"
+    target_names = ['Non-Search', 'Search']
+    search_URL_name = (search_URL_name["URL name"][0].split('/')[-1]).split('.')[0]
+    nameCSV = search_URL_name+'.csv'
 
-data1_import = evaluation(Search_X_test,searchname,target_names,search_URL_name)
-data2_import = pd.read_csv(Search_searchpath)   
-data_merge = pd.concat([data1_import, data2_import],axis=1)
+    data1_import = evaluation(Search_X_test,searchname,target_names,search_URL_name)
+    data2_import = pd.read_csv(Search_searchpath)   
+    data_merge = pd.concat([data1_import, data2_import],axis=1)
 
-data_merge.to_csv(nameCSV, index = False)  
+    data_merge.to_csv(nameCSV, index = False)  
+
+search_GT = "/Users/mdjavedulferdous/Desktop/TiiS/Code/result/search/search_GT__1.csv"
+search_GT_df = pd.read_csv(search_GT)
+name_of_csv_sort = []
+for i in range(len(search_GT_df['URL name'])):
+    name_of_csv_sort.append((search_GT_df['URL name'][i].split('/')[-1]).split('.')[0])
+for i in name_of_csv_sort:
+    search_extract_model(i)
